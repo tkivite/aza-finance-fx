5466242a6a7046ba97870f564f579f745a87a42f
Titus <tkivite@gmail.com>
2022-10-13 17:39:22 +0300

    adds customer and transactions db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+def seed_customers
+    id = 0
+    10.times do 
+      Customer.create(
+        surname: "surname_#{id}",
+        othernames: "othernames_#{id}"        
+      )
+      id = id + 1
+    end
+  end
+ 
+  
+  def seed_transactions
+    customers = Customer.all 
+    amount = 100 
+    customers.each do |customer|
+      5.times do
+       p = Transaction.create(
+            inAmount: amount,
+            inCurrency: 'KES',
+            outAmount: amount+10,
+            outCurrency: 'USD',
+            dateOfTransaction: '20221017',
+            customer_id: customer.id,
+        ) 
+        amount++      
+      end
+    end
+  end
+
+  seed_customers
+seed_transactions
