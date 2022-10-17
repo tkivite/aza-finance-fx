510ed720ec011113a52bea1fd084602b6903be9e
Titus <tkivite@gmail.com>
2022-10-17 15:37:50 +0300

    added config file

---
+++ config/initializers/dev_env.rb
@@ -0,0 +1,4 @@
+ENV["DEV_DB_URL"] = "postgres://postgres:postgres@localhost/aza_finance_fx_development"
+ENV["TEST_DB_URL"] = "postgres://postgres:postgres@localhost/aza_finance_fx_test"
+ENV["PROD_DB_URL"] = "postgres://postgres:postgres@localhost/aza_finance_fx_production"
+ENV["DATA_PAGE_SIZE"] = "15"
